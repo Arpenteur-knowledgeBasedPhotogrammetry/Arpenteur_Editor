bc365a8a8fb582d07557f654f9b6a7b748162273
Yaaqoub Semlali <semlali.yaaqoub@gmail.com>
2018-06-12 14:42:50 +0200

    initial commit

---
+++ src/main/java/org/arpenteur/ontology/editor/App.java
@@ -0,0 +1,13 @@
+package org.arpenteur.ontology.editor;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
